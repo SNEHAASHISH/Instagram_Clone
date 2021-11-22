62e1729fc5c08c6dfdeeea504bbe287540cd5036
SNEHAASHISH <85332537+SNEHAASHISH@users.noreply.github.com>
2021-11-22 16:38:52 +0530

    Delete Frontend directory

--- Frontend/practice/React/test.js
+++
@@ -1,5 +0,0 @@
-let values = ["abcd" , "asfa","Asfa" , "Asfasg" , "ASasg" , "asgasg"];
-
-
-// spread operator
-console.log({ newValues:[...values , "asjkfbajksf" , "alkbsbfjkasf" , "askfhajksf"]  });
